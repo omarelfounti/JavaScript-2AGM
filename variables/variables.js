82f6fa8ef453bcdac54ea81a5b21040ef1e0dbb0
omarelfounti <elfountiomar@gmail.com>
2025-01-20 21:55:48 +0100

    Paso 1

---
+++ variables/variables.js
@@ -0,0 +1,18 @@
+ //Variables
+ let miVariable = (100*6)/2
+console.log(miVariable);
+
+//Concatenar
+
+let miVariableOne = "Super";
+let miVariableTwo = "califragi";
+let miVariableThree = "listico";
+let miVariableFour = "espia";
+let miVariableFive = "lidoso";
+
+console.log(miVariableOne + miVariableTwo + miVariableThree +miVariableFour + miVariableFive);
+
+
+
+
+
