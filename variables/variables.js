b5e0a6b64a5511aced45a30d3641d75bfb697d0b
omarelfounti <elfountiomar@gmail.com>
2025-01-20 23:06:44 +0100

    Paso 2

--- variables/variables.js
+++ variables/variables.js
@@ -12,6 +12,62 @@ let miVariableFive = "lidoso";
 
 console.log(miVariableOne + miVariableTwo + miVariableThree +miVariableFour + miVariableFive);
 
+/*
+++ suma uno
+- resta uno
+- niega el número
++= suma un valor
+-= resta un valor
+*= multiplica por un valor
+/= divide por un valor
+*/
+
+//Por ejemplo
+//let b = 5;
+//b++; // 6
+//let a +=b // esto es lo mismo que a = a + b
+
+/*
+! Operador NO. Devuelve el valor de verdad opuesto.
+&& Operador AND. Devuelve verdadero si cada operando es verdadero; de lo contrario, devuelve falso.
+|| Operador OR. Devuelve verdadero si al menos un operando es verdadero; de lo contrario, devuelve falso.
+*/
+console.log("Salesman" > "Salesman");
+console.log("Salesman" != "Salesman");
+
+let miOperador = 1*2==2 && 3*4 != 13 > 11;
+let miOperador2 = 1*2 == 2 && 3*4 == 13 > 11;
+
+console.log(miOperador);
+console.log(miOperador2);
+
+let persona = {
+    nombre: 'Maxi',
+    edad: 20,
+    conductor: null
+    };
+    
+console.log(persona.conductor = ((persona.edad >=18) ? "Si" : 'No'));
+
+//Conversiones
+console.log("5" + 5) // (1)
+console.log("5" - 1) // (2)
+console.log(10 * null) // (3)
+console.log("ocho" * 8) // (4)
+console.log(true == 0) // (5)
+
+
+
+
+
+
+
+    
+
+
+
+
+
 
 
 
