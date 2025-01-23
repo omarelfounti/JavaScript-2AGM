29261d46ffeb2ce8515150bfe9e345c88a9bd63e
omarelfounti <elfountiomar@gmail.com>
2025-01-23 23:43:28 +0100

    arrays

---
+++ Loops/arrays.js
@@ -0,0 +1,35 @@
+//Collecciones o listas
+let miArray = ["Leonardo", "Michaelangelo", "Donatello", "Raphael"];
+console.log(miArray);
+
+let numLoteria = [13, 36, 45, 57, 67, 14]; 
+const numLetras = ["a", "b", "c", 1, 2, 3];
+console.log(numLoteria);
+console.log(numLetras);
+
+//acceso a la lista
+let frutas = ["fresas 🍓", "arandanos 🫐", "platanos 🍌"];
+console.log(frutas[0]);
+
+let pokemon = ["Bulbasaur", "Charmander", "Newton"];
+pokemon[2] = "Pikachu";
+console.log(pokemon);
+
+let numbers = [1, 2, 3];
+numbers[3] = 4;
+console.log(numbers);
+
+let numbers = [1, 2, 3];
+numbers [5] = 88;
+console.log(numbers);
+
+
+
+
+
+
+
+
+
+
+
--- Loops/loops.js
+++ Loops/loops.js
@@ -1,6 +1,31 @@
+//continue
+
 for (let i = 0; i < 5; i++) {
     if (i == 1) {
       continue;
     }
     console.log(i);
   };
+//break
+
+  for (let i = 0; i < 5; i++) {
+    if (i == 3) {
+      break;
+    }
+    console.log(i);
+  };
+
+  // Ejemplo práctico
+
+  for (let i = 1; i <= 50; i++) {
+    if (i % 2 === 1) {
+      continue;
+    } else if (i === 42) {
+      console.log(i);
+      break;
+    } else {
+      console.log(i);
+    }
+  };
+
+
