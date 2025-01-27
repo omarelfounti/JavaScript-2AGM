49b8d854d02e71ad061649ba661f90ecaa797667
omarelfounti <elfountiomar@gmail.com>
2025-01-27 23:02:28 +0100

    arrays2 paso 2

--- Loops y Arrays/arrays2.js
+++ Loops y Arrays/arrays2.js
@@ -16,5 +16,42 @@ listaPeliculas.pop();
 listaPeliculas.push("El Gran Hotel Budapest");
 listaPeliculas.unshift("Parásitos", "El Viaje de Chihiro");
   
-  console.log(listaPeliculas);
+console.log(listaPeliculas);
+
+//Más
+const diosesGriegos = ["Zeus", "Hera", "Poseidón", "Apolo", "Hermes", "Dionisio", "Hades"];
+const elementoEliminadoInicio = diosesGriegos.shift();
+const elementoEliminadoFinal = diosesGriegos.pop();
+  
+console.log(elementoEliminadoInicio); 
+console.log(elementoEliminadoFinal); 
+console.log(diosesGriegos); 
+ 
+//Ejemplo
+const diosesGriegos1 = ["Zeus", "Hera", "Poseidón", "Apolo", "Hermes", "Dionisio", "Hades"];
+
+
+const elementoEliminadoInicio1 = diosesGriegos1.shift();
+
+const elementoEliminadoFina1 = diosesGriegos1.pop();
+
+diosesGriegos1.unshift("Afrodita", "Ares");
+
+diosesGriegos1.push("Artemisa", "Hefesto");
+
+const elementoEliminadoMedio = diosesGriegos.splice(2, 1)[0];
+
+diosesGriegos[3] = "Deméter";
+
+const mensajeEliminados = `Dioses eliminados: ${elementoEliminadoInicio1}, ${elementoEliminadoFinal1}, ${elementoEliminadoMedio}`;
+
+
+const mensajeRestantes = `Dioses restantes: ${diosesGriegos1.join(", ")}`;
+
+console.log(mensajeEliminados);
+console.log(mensajeRestantes);
+
+
+
+  
   
