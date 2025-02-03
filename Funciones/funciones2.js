2a5eedafc7208c2d983e6714c7aaec5ec341db30
omarelfounti <elfountiomar@gmail.com>
2025-02-03 23:41:01 +0100

    funciones2

---
+++ Funciones/funciones2.js
@@ -0,0 +1,25 @@
+//Funciones con Parametros
+function saludos(nombre) {
+    console.log("¡Hola! Mi nombre es " + nombre + ".");
+  }
+  
+  saludos("Marshall");
+
+//Ejemplo con Parametros
+function saludos(nombre, edad) {
+    console.log("¡Hola! Mi nombre es " + nombre + ".");
+    console.log("Tengo " + edad + " años.");
+  }
+  
+  saludos("Marshall", 30);
+
+//Accesibilidad
+function sumar(x, y) {
+    let respuesta = x + y;
+    return respuesta;
+  }
+  
+  console.log(respuesta);
+
+
+  
