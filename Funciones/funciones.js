7b640a0d6ce1c03efaf4bc8b08308e2f4e519cf9
omarelfounti <elfountiomar@gmail.com>
2025-02-03 22:50:44 +0100

    funciones

---
+++ Funciones/funciones.js
@@ -0,0 +1,45 @@
+//Funciones
+function holaMundo() {
+    console.log("Hola, Mundo!");
+  }
+  
+  holaMundo();
+
+//Ejemplo
+function saludos() {
+    const numeroAleatorio = Math.floor(Math.random() * 4) + 1;
+  
+    if (numeroAleatorio == 1){
+      console.log("¡Hola!");
+    } else if (numeroAleatorio == 2) {
+      console.log("¡Buenas!");
+    } else if (numeroAleatorio == 3) {
+      console.log("¿Qué tal?");
+    } else if (numeroAleatorio == 4) {
+      console.log("¡Saludos!");
+    } else {
+      console.log("¡Hey!");
+    }
+  }
+  
+  saludos();
+  saludos();
+  saludos();
+
+//Otro ejemplo
+function saludos() {
+    for (let i = 1; i <= 3; i++) {
+      console.log(i);
+    }
+    console.log("¡Hola a todos!");
+  }
+  
+  saludos();
+
+//return
+function holaMundo() {
+    return "¡Hola, Mundo! 🌎";
+  }
+  
+  holaMundo();
+
