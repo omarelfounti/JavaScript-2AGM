0204800cd9f2a797cfffa25924c19d71122ac38b
omarelfounti <elfountiomar@gmail.com>
2025-02-10 22:44:17 +0100

    objetos2 y script

---
+++ Objetos/objetos2.js
@@ -0,0 +1,11 @@
+const telefono = {
+    modelo: "iPhone 16 Pro Max",
+    marca: "Apple",
+    version: 11.6,
+    propietario: "Mila",
+    tonoDeLlamada: function () {
+      console.log("¡Rin, rin! ¡Rin, rin! 📲")
+    }
+  }
+
+  telefono.tonoDeLlamada();  
